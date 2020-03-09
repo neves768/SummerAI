5c420601639cfd7b48800f7eeaac3c3ddb3c1347
neves768 <chris_oboy@hotmail.com>
2020-03-09 16:53:37 -0300

    Add minus and times operators

--- calcs.py
+++ calcs.py
@@ -5,7 +5,21 @@ class logic:
     '''
     op = {}
     def __init__(self):
-        logic.op = {"+":logic.add}
+        logic.op = {
+            "+":logic.add,
+            "-":logic.sub,
+            "*":logic.times
+        }
+
+    @staticmethod
+    def times(string):
+        n = re.findall("([\*.0-9]{1,7})", string)
+        if(len(n) > 2):
+            for i in range(len(n)-1):
+                if(n[i+1] == "*"):
+                    return float(n[i])*float(n[i+2])
+        else:
+            return False
     
     @staticmethod
     def add(string):
@@ -13,13 +27,23 @@ class logic:
         if(len(n) > 2):
             for i in range(len(n)-1):
                 if(n[i+1] == "+"):
-                    return int(n[i])+int(n[i+2])
+                    return float(n[i])+float(n[i+2])
+        else:
+            return False
+
+    @staticmethod
+    def sub(string):
+        n = re.findall("([\-.0-9]{1,7})", string)
+        if(len(n) > 2):
+            for i in range(len(n)-1):
+                if(n[i+1] == "-"):
+                    return float(n[i])-float(n[i+2])
         else:
             return False
 
     def routeToOp(self, string):
         for i, f in logic.op.items():
-            if(string.find(i)):
+            if(string.find(i) != -1):
                 return f(string)
 
 class A:
